25e86d2a3bcb2f5296ad6731b15ed5a9b9ba6470
Gustavo Pereira <gustavo.oito@hotmail.com>
2021-08-25 15:27:30 -0300

    Commit Final

---
+++ interart/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using interart.Models;
+
+namespace interart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var func = new FuncionarioModel();
+            return View(func);
+        }
+
+    }
+}
